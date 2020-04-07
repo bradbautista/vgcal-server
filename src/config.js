811649720e2df4a15ab89d67d917ec21234ebbe7
unknown <bradbautista@gmail.com>
2020-04-07 15:37:57 -0400

    more heroku tests

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 17043,
   DATABASE_URL: process.env.DATABASE_URL || `postgresql://${process.env.DBUSER}:${process.env.DBPASS}@localhost/vgcal_db`,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || `postgresql://${process.env.DBUSER}:${process.env.DBPASS}@localhost/vgcal_test_db`,
-  EMAILPASS: "vYNpz0kMBR9q25Kj",
-  EMAILUSER: "bradbautista@gmail.com"
+  EMAILPASS: process.env.EMAILPASS,
+  EMAILUSER: process.env.EMAILUSER
 }
--- src/server.js
+++ src/server.js
@@ -25,11 +25,11 @@ app.set('db', db)
 // second ( optional )
 
 // Note Heroku server is +4 hrs from EST/EDT
-const hour = 18; // 00 - 23
-const minute = '15'; // 00 - 59; str for leading 0s
+const hour = 19; // 00 - 23
+const minute = '45'; // 00 - 59; str for leading 0s
 
 // Want to add a year to the database? Add a year to this array
-const years = ['2020', '2021'];
+const years = ['2019'];
 
 // Sequentially schedule updates and backups of data 
 // for the years in the array above
