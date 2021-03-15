1a59f49f4a6305fa7979a6a3330511d23dce0e2c
unknown <bradbautista@gmail.com>
2021-03-15 19:46:25 -0400

    bumped deps, added 2022 to collection scope

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const knex = require('knex');
 const app = require('./app');
 const { PORT, DATABASE_URL, EMAILUSER } = require('./config');
-const cron = require("node-cron");
+const cron = require('node-cron');
 const serviceFunctions = require('./serviceFunctions');
 const moment = require('moment');
 
@@ -29,39 +29,43 @@ const hour = 01; // 00 - 23
 const minute = '20'; // 00 - 59; str for leading 0s
 
 // Want to add a year to the database? Add a year to this array
-// No need to update <= 2019 regularly, but 2020 & 2021 should be
-// updated daily
-const years = ['2020', '2021'];
+const years = ['2021', '2022'];
 
-// Sequentially schedule updates and backups of data 
+// Sequentially schedule updates and backups of data
 // for the years in the array above
 years.forEach((year, i) => {
+  console.log(
+    `Scheduling refresh of ${year} releases for ${
+      hour > 12 ? hour - 12 + i : hour + i
+    }:${minute} ${hour > 11 ? 'p.m.' : 'a.m.'} `
+  );
 
-  console.log(`Scheduling refresh of ${year} releases for ${(hour > 12) ? (hour - 12) + i : hour + i}:${minute} ${(hour > 11) ? 'p.m.' : 'a.m.' } `);
+  cron.schedule(`00 ${minute} ${hour + i} * * *`, function () {
+    console.log(`Refreshing ${year} releases`);
+    serviceFunctions.fetchAndManageReleasesByYear(db, year);
+  });
 
-    cron.schedule(`00 ${minute} ${hour + i} * * *`, function() {
+  console.log(
+    `Scheduling transmission of backup of ${year} releases for ${
+      hour > 12 ? hour - 12 + i : hour + i
+    }:${minute} ${hour > 11 ? 'p.m.' : 'a.m.'} and 15 seconds`
+  );
 
-      console.log(`Refreshing ${year} releases`);
-      serviceFunctions.fetchAndManageReleasesByYear(db, year);
+  cron.schedule(`15 ${minute} ${hour + i} * * *`, function () {
+    console.log(
+      `It's ${hour > 12 ? hour - 12 + i : hour + i}:${minute} ${
+        hour > 11 ? 'p.m.' : 'a.m.'
+      }, e-mailing daily backup of records for ${year} to ${EMAILUSER}`
+    );
 
-    });
+    // If we try to chain emailFile off any of the promises in the above-
+    // scheduled function, or run it elsewhere in the function, or with
+    // a setTimeout, it either jumps the gun or doesn't work, so we're waiting
+    // via a separate cron job and adding some time as padding
 
-  console.log(`Scheduling transmission of backup of ${year} releases for ${(hour > 12) ? (hour - 12) + i : hour + i}:${minute} ${(hour > 11) ? 'p.m.' : 'a.m.' } and 15 seconds`);
-
-    cron.schedule(`15 ${minute} ${hour + i} * * *`, function() {
-
-      console.log(`It's ${(hour > 12) ? (hour - 12) + i : hour + i}:${minute} ${(hour > 11) ? 'p.m.' : 'a.m.' }, e-mailing daily backup of records for ${year} to ${EMAILUSER}`);
-
-      // If we try to chain emailFile off any of the promises in the above-
-      // scheduled function, or run it elsewhere in the function, or with
-      // a setTimeout, it either jumps the gun or doesn't work, so we're waiting
-      // via a separate cron job and adding some time as padding
-
-      serviceFunctions.emailFile(year);
-
-    });
-
-})
+    serviceFunctions.emailFile(year);
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
