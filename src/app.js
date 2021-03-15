873f6c513997552a4976e46469f08d541225869e
unknown <bradbautista@gmail.com>
2021-03-15 19:47:47 -0400

    Removed unused import

--- src/app.js
+++ src/app.js
@@ -7,13 +7,14 @@ const moment = require('moment');
 const { NODE_ENV } = require('./config');
 const releasesRouter = require('../releases/releases-router');
 const favoritesRouter = require('../favorites/favorites-router');
-const serviceFunctions = require('./serviceFunctions');
 
-const app = express()
+const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test',
-  }));
+  })
+);
 app.use(cors());
 app.use(helmet());
 
@@ -21,14 +22,14 @@ app.use('/api/releases', releasesRouter);
 app.use('/api/favorites', favoritesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-      response = { error: 'Server error' }
-    } else {
-      console.error(error)
-      response = { error: error.message, object: error }
-    }
-    res.status(500).json(response)
-  })
-  
-module.exports = app
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: 'Server error' };
+  } else {
+    console.error(error);
+    response = { error: error.message, object: error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
