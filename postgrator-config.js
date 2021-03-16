5ee8963facb9ce8ff92404905b34ac760e1d3542
unknown <bradbautista@gmail.com>
2021-03-15 20:29:35 -0400

    more ssl tweaks

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,5 +7,7 @@ module.exports = {
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
-  ssl: true,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
